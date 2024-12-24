d86696c386c53b5a4256ce5b632ea51796949346
Revanth Kanna M <revanthkannam05@gmail.com>
2024-12-24 13:08:28 +0530

    documentation

--- server/index.js
+++ server/index.js
@@ -1,22 +1,32 @@
 import express from "express";
 import cors from "cors"
 import bodyParser from "body-parser";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+
+
+
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(__dirname+'/public'))
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json())
 const port = 3000;
 let notes = [];
 app.get("/",(req,res)=>{
+    res.sendFile(path)
+})
+app.get("/api",(req,res)=>{
     res.json(notes);
 })
 
-app.post("/",(req,res)=>{
+app.post("/api",(req,res)=>{
     console.log(req.body);
      notes.push(req.body);
      res.sendStatus(201);
 })
-app.delete("/",(req,res)=>{
+app.delete("/api",(req,res)=>{
     const {key} = req.body;
     console.log(key);
     notes.splice(key,1);
