38685f1a20bf9f1d5c4223384133b212258aad4a
Revanth Kanna M <revanthkannam05@gmail.com>
2024-12-27 16:03:37 +0530

    protected routing 8

--- server/index.js
+++ server/index.js
@@ -96,8 +96,16 @@ app.post("/login", (req, res, next) => {
                 console.error("Login Error:", error);
                 return  res.sendStatus(500);
             } else {
+                res.cookie("connect.sid", req.sessionID, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === "production", // Secure cookies in production
+                    sameSite: "None", // For cross-site cookie sharing
+                    maxAge: 86400000 // Cookie expiration (1 day)
+                });
+
+                // Send a successful response after setting the cookie
+                res.sendStatus(200);
                 console.log("Login Successful");
-                res.sendStatus(200);  
             }
         });
     })(req, res, next); // Pass req, res, and next to allow proper flow
